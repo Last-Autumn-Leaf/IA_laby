c130ff23fab03d6573a27defb4f3bfffc7a9040c
TheShyDev-Yoogo <yoogo.production@gmail.com>
2022-09-12 01:07:55 -0400

    changes genetic v3.0

--- IA_controller/Fuzzy_logic.py
+++ IA_controller/Fuzzy_logic.py
@@ -1,5 +1,4 @@
 from IA_controller.Helper_fun import setCorrectCHWD
-from IA_controller.PrologCom import PrologCom
 from IA_controller.visualizer import App_2
 
 import skfuzzy as fuzz
@@ -8,155 +7,129 @@ import numpy as np
 
 import matplotlib.pyplot as plt
 
-#pour que le fuzzy controller puisse bouger le bonhomme
-from Games2D import *
+class FuzzPlayer:
+    membership_naming = "μ_"
+    def __init__(self,tile_size):
+        self.tile_size=tile_size
 
-membership_naming="μ_"
-def create_membership_function(ctrl_ant,value,name=membership_naming,n_function=5,saturation=True):
-    assert(n_function>=1)
-    vecteur1 = [x-int(n_function/2) +(0 if not n_function%2==0 else 0.5) for x in range(n_function)]
-    for i in range(n_function):
-        if saturation and (i==0 or i==n_function-1) :
+    def create_membership_function(self,ctrl_ant, value, name=membership_naming, n_function=5, saturation=True):
+        assert (n_function >= 1)
+        vecteur1 = [x - int(n_function / 2) + (0 if not n_function % 2 == 0 else 0.5) for x in range(n_function)]
+        for i in range(n_function):
+            if saturation and (i == 0 or i == n_function - 1):
 
-            if i==0:
-                ctrl_ant[name + str(i)] = fuzz.trapmf(ctrl_ant.universe,
-                    [value * (vecteur1[i] - 2),value * (vecteur1[i] - 1), value * vecteur1[i],value * (vecteur1[i] + 1)])
+                if i == 0:
+                    ctrl_ant[name + str(i)] = fuzz.trapmf(ctrl_ant.universe,
+                                                          [value * (vecteur1[i] - 2), value * (vecteur1[i] - 1),
+                                                           value * vecteur1[i], value * (vecteur1[i] + 1)])
 
-            if i==n_function-1:
-                ctrl_ant[name + str(i)] = fuzz.trapmf(ctrl_ant.universe,
-                    [value * (vecteur1[i] - 1),value * (vecteur1[i] ), value * (vecteur1[i] + 1),value * (vecteur1[i] + 2)])
-        else:
-            ctrl_ant[name+str(i)]= fuzz.trimf(ctrl_ant.universe, [value*(vecteur1[i]-1), value*vecteur1[i], value*(vecteur1[i]+1)])
+                if i == n_function - 1:
+                    ctrl_ant[name + str(i)] = fuzz.trapmf(ctrl_ant.universe,
+                                                          [value * (vecteur1[i] - 1), value * (vecteur1[i]),
+                                                           value * (vecteur1[i] + 1), value * (vecteur1[i] + 2)])
+            else:
+                ctrl_ant[name + str(i)] = fuzz.trimf(ctrl_ant.universe, [value * (vecteur1[i] - 1), value * vecteur1[i],
+                                                                         value * (vecteur1[i] + 1)])
 
-def create_rule(rule_matrix,input_A,input_B,output,operator='and',name=membership_naming):
-    rules = []
+    def create_rule(self,rule_matrix, input_A, input_B, output, operator='and', name=membership_naming):
+        rules = []
+        for i in range(len(rule_matrix)):
+            for j in range(len(rule_matrix)):
+                if operator == 'and':
+                    rules.append(ctrl.Rule(antecedent=(input_A[name + str(i)] & input_B[name + str(j)]),
+                                           consequent=output[name + str(rule_matrix[i][j])]))
+                elif operator == 'or':
+                    rules.append(ctrl.Rule(antecedent=(input_A[name + str(i)] | input_B[name + str(j)]),
+                                           consequent=output[name + str(rule_matrix[i][j])]))
 
-    for i in range(len(rule_matrix)):
-        for j in range(len(rule_matrix)):
-            if operator=='and':
-                rules.append(ctrl.Rule(antecedent=(input_A[name+str(i)] & input_B[name+str(j)]), consequent=output[name+str(rule_matrix[i][j])]))
-            elif operator=='or':
-                rules.append(ctrl.Rule(antecedent=(input_A[name+str(i)] | input_B[name+str(j)]), consequent=output[name+str(rule_matrix[i][j])]))
+        return rules
 
-    return rules
+    def createFuzzyController(self):
+        # TODO: Create the fuzzy variables for inputs and outputs.
+        # Defuzzification (defuzzify_method) methods for fuzzy variables:
+        #    'centroid': Centroid of area
+        #    'bisector': bisector of area
+        #    'mom'     : mean of maximum
+        #    'som'     : min of maximum
+        #    'lom'     : max of maximum
+        self.tile_size = 100
+        DtoGoal = ctrl.Antecedent(np.linspace(-2 * self.tile_size, 2 * self.tile_size, 1000), 'DtoGoal')
+        DtoObst = ctrl.Antecedent(np.linspace(-2 * self.tile_size, 2 * self.tile_size, 1000), 'DtoObstacle')
+        Pcommand = ctrl.Consequent(np.linspace(-100, 100, 1000), 'PlayerCommand', defuzzify_method='centroid')
 
-def createFuzzyController():
-    # TODO: Create the fuzzy variables for inputs and outputs.
-    # Defuzzification (defuzzify_method) methods for fuzzy variables:
-    #    'centroid': Centroid of area
-    #    'bisector': bisector of area
-    #    'mom'     : mean of maximum
-    #    'som'     : min of maximum
-    #    'lom'     : max of maximum
-    DtoGoal = ctrl.Antecedent(np.linspace(-1, 1, 1000), 'DtoGoal')
-    DtoObst = ctrl.Antecedent(np.linspace(-50, 50, 1000), 'DtoObstacle')
-    Pcommand = ctrl.Consequent(np.linspace(-10, 10, 1000), 'PlayerCommand', defuzzify_method='centroid')
+        # Accumulation (accumulation_method) methods for fuzzy variables:
+        #    np.fmax
+        #    np.multiply
+        Pcommand.accumulation_method = np.fmax
 
-# Accumulation (accumulation_method) methods for fuzzy variables:
-    #    np.fmax
-    #    np.multiply
-    Pcommand.accumulation_method = np.fmax
+        self.create_membership_function(DtoGoal, self.tile_size, n_function=3)
+        self.create_membership_function(DtoObst, self.tile_size, n_function=3)
+        self.create_membership_function(Pcommand, 50, n_function=3, saturation=False)
 
-    # TODO: Create membership functions
-    DtoGoal['neg'] = fuzz.trapmf(DtoGoal.universe, [-0.25, -0.25, -0.1, -0.05])
-    DtoGoal['zero'] = fuzz.trimf(DtoGoal.universe, [-0.05, 0, 0.05])
-    DtoGoal['pos'] = fuzz.trapmf(DtoGoal.universe, [0.05, 0.1, 0.25, 0.25])
+        '''DtoGoal['neg'] = fuzz.trapmf(DtoGoal.universe, [-0.25, -0.25, -0.1, -0.05])
+        DtoGoal['zero'] = fuzz.trimf(DtoGoal.universe, [-0.05, 0, 0.05])
+        DtoGoal['pos'] = fuzz.trapmf(DtoGoal.universe, [0.05, 0.1, 0.25, 0.25])
 
-    DtoObst['neg'] = fuzz.trapmf(DtoObst.universe, [-100, -100, -10, -2])
-    DtoObst['zero'] = fuzz.trimf(DtoObst.universe, [-2, 0, 2])
-    DtoObst['pos'] = fuzz.trapmf(DtoObst.universe, [2, 10, 100, 100])
+        DtoObst['neg'] = fuzz.trapmf(DtoObst.universe, [-100, -100, -10, -2])
+        DtoObst['zero'] = fuzz.trimf(DtoObst.universe, [-2, 0, 2])
+        DtoObst['pos'] = fuzz.trapmf(DtoObst.universe, [2, 10, 100, 100])
 
-    Pcommand['neg'] = fuzz.trapmf(Pcommand.universe, [-10, -10, -5, -1])
-    Pcommand['zero'] = fuzz.trimf(Pcommand.universe, [-1, 0, 1])
-    Pcommand['pos'] = fuzz.trapmf(Pcommand.universe, [1, 5, 10, 10])
+        Pcommand['neg'] = fuzz.trapmf(Pcommand.universe, [-10, -10, -5, -1])
+        Pcommand['zero'] = fuzz.trimf(Pcommand.universe, [-1, 0, 1])
+        Pcommand['pos'] = fuzz.trapmf(Pcommand.universe, [1, 5, 10, 10])'''
 
-    # TODO: Define the rules.
-    rules = []
-    rules.append(ctrl.Rule(antecedent=(DtoGoal['neg'] & DtoObst['neg']), consequent=Pcommand['zero']))
-    rules.append(ctrl.Rule(antecedent=(DtoGoal['neg'] & DtoObst['pos']), consequent=Pcommand['neg']))
-    rules.append(ctrl.Rule(antecedent=(DtoGoal['neg'] & DtoObst['zero']), consequent=Pcommand['neg']))
+        RULES_MATRIX = [[1, 0, 0],
+                        [2, 1, 0],
+                        [2, 2, 1]]
+        rules = self.create_rule(RULES_MATRIX, DtoGoal, DtoObst, Pcommand)
+        '''rules.append(ctrl.Rule(antecedent=(DtoGoal['neg'] & DtoObst['neg']), consequent=Pcommand['zero']))
+        rules.append(ctrl.Rule(antecedent=(DtoGoal['neg'] & DtoObst['pos']), consequent=Pcommand['neg']))
+        rules.append(ctrl.Rule(antecedent=(DtoGoal['neg'] & DtoObst['zero']), consequent=Pcommand['neg']))
 
-    rules.append(ctrl.Rule(antecedent=(DtoGoal['zero'] & DtoObst['neg']), consequent=Pcommand['pos']))
-    rules.append(ctrl.Rule(antecedent=(DtoGoal['zero'] & DtoObst['pos']), consequent=Pcommand['neg']))
-    rules.append(ctrl.Rule(antecedent=(DtoGoal['zero'] & DtoObst['zero']), consequent=Pcommand['zero']))
+        rules.append(ctrl.Rule(antecedent=(DtoGoal['zero'] & DtoObst['neg']), consequent=Pcommand['pos']))
+        rules.append(ctrl.Rule(antecedent=(DtoGoal['zero'] & DtoObst['pos']), consequent=Pcommand['neg']))
+        rules.append(ctrl.Rule(antecedent=(DtoGoal['zero'] & DtoObst['zero']), consequent=Pcommand['zero']))
 
-    rules.append(ctrl.Rule(antecedent=(DtoGoal['pos'] & DtoObst['neg']), consequent=Pcommand['pos']))
-    rules.append(ctrl.Rule(antecedent=(DtoGoal['pos'] & DtoObst['pos']), consequent=Pcommand['zero']))
-    rules.append(ctrl.Rule(antecedent=(DtoGoal['pos'] & DtoObst['zero']), consequent=Pcommand['pos']))
+        rules.append(ctrl.Rule(antecedent=(DtoGoal['pos'] & DtoObst['neg']), consequent=Pcommand['pos']))
+        rules.append(ctrl.Rule(antecedent=(DtoGoal['pos'] & DtoObst['pos']), consequent=Pcommand['zero']))
+        rules.append(ctrl.Rule(antecedent=(DtoGoal['pos'] & DtoObst['zero']), consequent=Pcommand['pos']))'''
 
-    # Conjunction (and_func) and disjunction (or_func) methods for rules:
-    #     np.fmin
-    #     np.fmax
-    for rule in rules:
-        rule.and_func = np.fmin
-        rule.or_func = np.fmax
+        for rule in rules:
+            rule.and_func = np.fmin
+            rule.or_func = np.fmax
 
-    system = ctrl.ControlSystem(rules)
-    sim = ctrl.ControlSystemSimulation(system)
-    return sim
+        system = ctrl.ControlSystem(rules)
+        self.sim = ctrl.ControlSystemSimulation(system)
+        return self.sim
 
+    def getOutput(self,input1,input2):
+        self.sim.input['DtoGoal'] = input1
+        self.sim.input['DtoObstacle'] = input2
+        self.sim.compute()
 
+        force = self.sim.output['PlayerCommand']
+        return force
 
-def fuzzy_command(pos_joueur, pos_obstacle):
-    pass
-    pos_joueur = ...
-    pos_obstacle =...
-    taille_obstacle =...
-    #return pwm_command
-
-
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    # Createfuzzy controller
-    fuzz_ctrl = createFuzzyController()
-
-    # Display rules
-    print('------------------------ RULES ------------------------')
-    for rule in fuzz_ctrl.ctrl.rules:
-        print(rule)
-    print('-------------------------------------------------------')
 
     # Display fuzzy variables
-    for var in fuzz_ctrl.ctrl.fuzzy_variables:
+    '''for var in fuzz_ctrl.ctrl.fuzzy_variables:
         var.view()
-    plt.show()
+    plt.show()'''
 
-    VERBOSE = False
 
     #TODO definir une action initiale cohérente
-    action = np.array([1.0], dtype=np.float32)
+
 
     setCorrectCHWD()
     map_file_name = 'assets/test_Map'
     theAPP = App_2(map_file_name)
 
-    for _ in range(1000):
 
-        # Execute the action
-        instruction_AI = action
-        #TODO convertir l'action en instruction pour deplacer le joueur
-        # theAPP.on_AI_input(instruction_AI)
-
-        # if done:
-        #     # End the episode
-        #     isSuccess = False
-        #     break
-
-        # Select the next action based on the observation
-        # cartPosition, cartVelocity, poleAngle, poleVelocityAtTip = observation
-
-        # TODO: set the input to the fuzzy system
-        fuzz_ctrl.input['DtoGoal'] = 0
-        fuzz_ctrl.input['DtoObstacle'] = 0
-
-        fuzz_ctrl.compute()
-        if VERBOSE:
-            fuzz_ctrl.print_state()
-
-        # TODO: get the output from the fuzzy system
-        force = fuzz_ctrl.output['PlayerCommand']
-
-        action = np.array(force, dtype=np.float32).flatten()
-
+    fuzz_ctrl_x=FuzzPlayer(theAPP.maze.tile_size_x)
+    fuzz_ctrl_y=FuzzPlayer(theAPP.maze.tile_size_y)
+    fuzz_ctrl_x.createFuzzyController()
+    fuzz_ctrl_y.createFuzzyController()
+    theAPP.setIA_controller(fuzz_ctrl_x.getOutput,fuzz_ctrl_y.getOutput)
 
     theAPP.on_execute()
--- IA_controller/visualizer.py
+++ IA_controller/visualizer.py
@@ -1,5 +1,6 @@
 import pygame
 
+from Constants import GAME_CLOCK
 from Games2D import App
 from IA_controller.Helper_fun import draw_rect_alpha
 
@@ -8,6 +9,8 @@ class App_2 (App):
     def __init__(self,mazefile):
         super().__init__(mazefile)
         self.visited_cases=[]
+        self.IA_controller_X=None
+        self.IA_controller_Y=None
 
     def on_render(self):
         self.maze_render()
@@ -27,3 +30,82 @@ class App_2 (App):
 
     def set_visited(self,v):
         self.visited_cases=v
+
+    def setIA_controller(self,controllerXfun,controllerYfun):
+        self.IA_controller_X=controllerXfun
+        self.IA_controller_Y=controllerYfun
+
+
+    def doForce_Y(self,force):
+        for i in range(int(force / 10)):
+            if force > 0:
+                self.on_AI_input('DOWN')
+            else:
+                self.on_AI_input('UP')
+
+    def doForce_X(self,force):
+        for i in range(int(force / 10)):
+            if force > 0:
+                self.on_AI_input('RIGHT')
+            else:
+                self.on_AI_input('LEFT')
+    def on_execute(self):
+        self.on_init()
+
+        while self._running:
+            self._clock.tick(GAME_CLOCK)
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    self._running = False
+                if event.type == pygame.USEREVENT:
+                    self.timer += 0.01
+            pygame.event.pump()
+            keys = pygame.key.get_pressed()
+            self.on_keyboard_input(keys)
+
+            #
+            if self.IA_controller_X is not None:
+
+                dG_y=self.maze.coinList[0].centery -self.player.y
+                dG_x=self.maze.coinList[0].centerx -self.player.x
+
+                dO_y=self.maze.obstacleList[0].centery - self.player.y
+                dO_x=self.maze.obstacleList[0].centerx -self.player.x
+
+                force_Y=self.IA_controller_Y(dG_y,dO_y)
+                force_X=self.IA_controller_X(dG_x,dO_x)
+                self.doForce_X(force_X)
+                self.doForce_Y(force_Y)
+
+            # self.on_AI_input(instruction)
+            #
+
+            if self.on_coin_collision():
+                self.score += 1
+            if self.on_treasure_collision():
+                self.score += 10
+            monster = self.on_monster_collision()
+            if monster:
+                if monster.fight(self.player):
+                    self.maze.monsterList.remove(monster)
+                else:
+                    self._running = False
+                    self._dead = True
+            if self.on_exit():
+                self._running = False
+                self._win = True
+            self.on_render()
+
+        while self._win:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    self._win = False
+            self.on_win_render()
+
+        while self._dead:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    self._dead = False
+            self.on_death_render()
+
+        self.on_cleanup()
