e45cbe3f8122538ff3e9744a84a55f2deb9b08a4
TheShyDev-Yoogo <yoogo.production@gmail.com>
2022-09-16 12:56:37 -0400

    Genetic implementation

--- main.py
+++ main.py
@@ -1,15 +1,21 @@
-# Simple interactive dungeon crawler
-# This code was written for the AI courses in computer engineering at Université de Sherbrooke
-# Author : Audrey Corbeil Therrien
-
-from Games2D import *
+from IA_controller.Fuzzy_logic import FuzzPlayer
+from IA_controller.Helper_fun import getMazeFromFile
+from IA_controller.Plannification_search import Plannificator
+from IA_controller.PrologCom import PrologCom
+from IA_controller.visualizer import App_2
 
 if __name__ == '__main__':
-    # Niveau 0 - sans obstacle - 'assets/mazeMedium_0'
-    # Niveau 1 - avec obstacles - 'assets/mazeMedium_1'
-    # Niveau 2 - avec obstacles et ennemis - 'assets/mazeMedium_2'
 
-    theAPP = App('assets/mazeMedium_4')
-    print(theAPP.maze.monsterList)
+    map_file_name = 'assets/MazeLarge_3'
+    maze = getMazeFromFile(map_file_name)
+    plannificator = Plannificator(PrologCom(maze))
+
+    theAPP = App_2(mazefile=map_file_name, plannificator=plannificator)
+
+    ### Integration de fuzzy ###
+    tile_size = (theAPP.maze.tile_size_x, theAPP.maze.tile_size_y)
+    fuzz_ctrl = FuzzPlayer(tile_size)
+    theAPP.setFuzzCtrl(fuzz_ctrl)
+    ### --- ###
+
     theAPP.on_execute()
-
